96672a97f80ef583bf80bcd7ee859464d9d443e8
moofx19 <mahmoudofx@gmail.com>
2025-03-11 15:04:02 +0200

    Update DatabaseSeeder to call RolePermissionSeeder, MarketSeeder, and AreaSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            RolePermissionSeeder::class,
+            MarketSeeder::class,
+            AreaSeeder::class,
+        ]);
 
     }
 }

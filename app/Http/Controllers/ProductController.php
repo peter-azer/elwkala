6071d4cf65a253169a6442756789b13a2c093438
moofx19 <mahmoudofx@gmail.com>
2025-07-03 15:15:02 +0300

    Refactor index method in ProductController to include user's cart items and attach cart quantity to products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Cart;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 
@@ -13,10 +14,29 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::where('hide', false)
-        ->with('subCategory', 'brand', 'productsPacksSizes', 'productsPacksSizes.productsPacks')
-        ->get();
-        return response()->json($products);
+        $user = auth()->user();
+        $market_id = $user->market()->first()->id;
+
+        // Get the user's cart items (not ordered)
+        $cartItems = Cart::where('market_id', $market_id)
+            ->where('ordered', false)
+            ->get()
+            ->keyBy('products_packs_sizes_id');
+
+        // Get all products with relations
+        $products = Product::with(['subCategory', 'brand', 'productsPacksSizes', 'productsPacksSizes.productsPacks'])->get();
+
+        // Attach cart quantity to each products_packs_size if it exists in the cart
+        $products = $products->map(function ($product) use ($cartItems) {
+            if ($product->productsPacksSizes) {
+                foreach ($product->productsPacksSizes as $pps) {
+                    $pps->cart_quantity = $cartItems->has($pps->id) ? $cartItems[$pps->id]->quantity : 0;
+                }
+            }
+            return $product;
+        });
+
+        return response()->json($products, 200);
     }
 
     /**

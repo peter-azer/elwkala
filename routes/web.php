580a87d4392fc593940a24b0bd756ccd186d94f7
moofx19 <mahmoudofx@gmail.com>
2025-10-14 12:28:44 +0300

    add privacy policy page and route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/policy', function () {
+    return view('policy');
+})->name('policy');

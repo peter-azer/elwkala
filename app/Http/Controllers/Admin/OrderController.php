48cb45091cdf2392ede0ebefc9a996cb57368f29
moofx19 <mahmoudofx@gmail.com>
2025-11-19 17:16:21 +0200

    test

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -18,7 +18,7 @@ class OrderController extends Controller
     {
         try {
             $orders = Order::query()
-                ->with('market', 'assignedOrders.name', 'product', 'product.productsPacksSizes')
+                ->with('market', 'assignedOrders.user[name]', 'product', 'product.productsPacksSizes')
                 ->get();
             return response()->json($orders);
         } catch (\Exception $error) {
